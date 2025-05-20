64ff3da32bff03869c81a1514fe9cc838b1667a1
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-20 00:46:49 -0300

    Setup api helpers

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,9 @@
+export const BASE_URL = 'http://localhost:3000';
+export const API_URL = `${BASE_URL}/api`;
+
+export function getHeaders() {
+	const token = '';
+	return {
+		Authorization: `Bearer ${token}`
+	};
+}
