0d9ce4174b05c06241c45f505d41e07b125b0709
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-19 23:06:13 -0300

    Add require role middleware

--- backend/middleware/auth.go
+++ backend/middleware/auth.go
@@ -1,11 +1,14 @@
 package middleware
 
 import (
+	"pokemon/database"
+	"pokemon/models"
 	"pokemon/utils"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func Protected() fiber.Handler {
@@ -24,5 +27,25 @@ func Protected() fiber.Handler {
         claims := token.Claims.(jwt.MapClaims)
         c.Locals("user_id", uint(claims["user_id"].(float64)))
         return c.Next()
+    }
+}
+
+func RequireRole(roles ...string) fiber.Handler {
+    return func(c *fiber.Ctx) error {
+        userID := c.Locals("user_id").(uuid.UUID)
+        var user models.User
+        if err := database.DB.First(&user, "id = ?", userID).Error; err != nil {
+            return c.SendStatus(fiber.StatusUnauthorized)
+        }
+
+        for _, r := range roles {
+            if user.Role == r {
+                return c.Next()
+            }
+        }
+
+        return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+            "error": "Insufficient permissions",
+        })
     }
 }
