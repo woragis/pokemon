71940b0996a4c0f878877add5eeb3c555af8d679
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-20 01:32:52 -0300

    Import navbar into layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-	import '../app.css';
+	import Navbar from '@/components/Navbar.svelte';
+	import '@/app.css';
 
 	let { children } = $props();
 </script>
 
+<Navbar />
 {@render children()}
