6bd6c48cee9d3187138c2b74c9c0997b84b4eef8
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-20 16:36:12 -0300

    Remove @ alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,14 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import path from 'node:path';
 
 export default defineConfig({
-	// resolve: {
-	// 	alias: {
-	// 		'@': path.resolve(__dirname, './src')
-	// 	}
-	// },
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
