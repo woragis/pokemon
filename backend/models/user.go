041b8130e86de6e47eac94f935dc7547974d6d4d
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-19 23:16:49 -0300

    Update user model and add trainer model

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,12 +1,27 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type User struct {
-    gorm.Model
-    Username string `gorm:"unique;not null"`
-    Name     string `gorm:"null"`
-    Email    string `gorm:"unique;not null"`
-    Password string `gorm:"not null"`
-    Role     string `gorm:"default:user"` // "user", "moderator", "admin"
+	gorm.Model
+	ID       uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
+	Username string    `gorm:"unique;not null"`
+	Name     string
+	Email    string    `gorm:"unique;not null"`
+	Password string    `gorm:"not null"`
+	Role     string    `gorm:"default:user"`
+
+	// Trainer  Trainer   `gorm:"constraint:OnDelete:CASCADE"` // establishes relation
+}
+
+type Trainer struct {
+	ID       uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
+	UserID   uuid.UUID `gorm:"type:uuid;not null;unique"`
+	Nickname string    `gorm:"not null"`
+	Avatar   string
+
+	User     User      `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
 }
