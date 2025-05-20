13be43caa1b2fdfa3709615a0127e2f76dd5b990
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-20 16:26:57 -0300

    Update default layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
-	import Navbar from '@/components/Navbar.svelte';
 	import '@/app.css';
+	import Navbar from '@/components/Navbar.svelte';
+	import Footer from '@/components/Footer.svelte';
 
 	let { children } = $props();
 </script>
 
 <Navbar />
+<main id="main-content">
 {@render children()}
+</main>
+<Footer />
+
+<style>
+	#main-content {
+		min-height: calc(100vh - 80px);
+	}
+</style>
