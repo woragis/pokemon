f9209c7c68395d3ba03fb06b022c25a7e0822c01
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-20 16:24:51 -0300

    Create guide by id page

---
+++ frontend/src/routes/guides/[id]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { page } from '$app/stores';
+	import { getGuide } from '@/api/guides.js';
+
+	const guide = getGuide($page.params.id);
+</script>
+
+<h1 class="text-2xl font-bold mb-2">{guide.title}</h1>
+<p class="text-gray-500 mb-4">{guide.category}</p>
+
+<div class="prose max-w-none">
+	{@html guide.content}
+</div>
