197d8891654a99bb606f3cd945ce1e6dce6a7f5e
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-19 22:17:07 -0300

    Create game guide routes

---
+++ backend/routes/game_guide.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"pokemon/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterGameGuideRoutes(app *fiber.App) {
+	guides := app.Group("/guides")
+
+	guides.Post("/", controllers.CreateGameGuide)
+	guides.Get("/", controllers.ListGameGuides)
+	guides.Get("/:slug", controllers.GetGameGuide)
+	guides.Put("/:slug", controllers.UpdateGameGuide)
+
+}
