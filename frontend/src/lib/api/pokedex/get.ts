76149bf0a6fa9aa7b72f11205fd9f3ed251231c9
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-21 20:26:18 -0300

    Create pokemons api

--- frontend/src/lib/api/pokedex/get.ts
+++ frontend/src/lib/api/pokedex/get.ts
@@ -1,14 +1,40 @@
-import { POKEDEX_URL, type PokedexResponse } from '.';
-import { getHeaders } from '..';
+import { pokemons, total } from '$lib/store/pokemons';
+import type { PokemonData } from '$lib/types/pokeapi';
+import { POKEMONS_POKEAPI } from '.';
 
-export async function fetchPokedex() {
+export interface Pagination {
+	offset: number;
+	limit: number;
+}
+type Common = {
+	name: string;
+	url: string;
+};
+type Pokemon = Common;
+interface AllPokemonsResponse {
+	count: number;
+	results: Pokemon[];
+}
+
+export async function fetchAllPokemons({ offset = 0, limit = 100 }: Pagination) {
 	try {
-		const res = await fetch(`${POKEDEX_URL}/`, {
-			headers: getHeaders()
-		});
-		if (!res.ok) throw new Error();
-		return (await res.json()) as PokedexResponse;
+		const res = await fetch(`${POKEMONS_POKEAPI}?offset=${offset}&limit=${limit}`);
+		if (!res.ok) throw new Error('Failed to fetch Pokémon list');
+		const response: AllPokemonsResponse = await res.json();
+		total.set(response.count);
+
+		const details = await Promise.all(
+			response.results.map(async (pokemon) => {
+				const res = await fetch(pokemon.url);
+				if (!res.ok) throw new Error('Failed to fetch Pokémon data');
+				return (await res.json()) as PokemonData;
+			})
+		);
+
+		pokemons.set(details); // use `.set()` instead of push
+
+		return response;
 	} catch (e: any) {
-		throw new Error('Error fetching pokedex');
+		throw new Error('Error fetching Pokédex');
 	}
 }
--- frontend/src/lib/api/pokedex/index.ts
+++ frontend/src/lib/api/pokedex/index.ts
@@ -1,11 +1,13 @@
 import { API_URL } from '..';
 import type { DefaultResponse } from '$lib/types';
 import type { PokedexPokemon } from '$lib/types/pokemon';
-import { fetchPokedex } from './get';
+import { fetchAllPokemons } from './get';
 export const POKEDEX_URL = `${API_URL}/pokedex`;
+export const POKEAPI = 'https://pokeapi.co/api/v2';
+export const POKEMONS_POKEAPI = `${POKEAPI}/pokemon`;
 
 export interface PokedexResponse extends DefaultResponse {
 	pokemons: PokedexPokemon[];
 }
 
-export { fetchPokedex };
+export { fetchAllPokemons };
