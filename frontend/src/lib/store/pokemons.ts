951cbb99a31ec79c53516cd8918279ca047c3302
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-21 20:53:04 -0300

    Update pokemon store

--- frontend/src/lib/store/pokemons.ts
+++ frontend/src/lib/store/pokemons.ts
@@ -1,7 +1,17 @@
 import type { Pagination } from '$lib/api/pokedex/get';
 import type { PokemonData } from '$lib/types/pokeapi';
-import { writable } from 'svelte/store';
+import { derived, writable } from 'svelte/store';
 
 export const pokemons = writable<PokemonData[]>([]);
-export const total = writable<number>(0);
+export const total = 1025;
 export const pagination = writable<Pagination>({ offset: 0, limit: 20 });
+export const pageNumbers = derived([pagination], ([$pagination]) => {
+	const totalPages = Math.ceil(total / $pagination.limit);
+	const currentPage = $pagination.offset / $pagination.limit + 1;
+
+	let start = Math.max(1, currentPage - 2);
+	let end = Math.min(totalPages, start + 4);
+	start = Math.max(1, end - 4); // shift range back if near the end
+
+	return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+});
