17ca38fa92443e501279b0717e74c2d1a5283cab
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-23 01:24:29 -0300

    Create permission and role models

--- backend/models/user.go
+++ backend/models/user.go
@@ -6,23 +6,19 @@ import (
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
-
 type User struct {
-	ID       uuid.UUID         `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
-	Username string            `gorm:"unique;not null" json:"username"`
-	Name     string            `json:"name"`
-	Email    string            `gorm:"unique;not null" json:"email"`
-	Password string            `gorm:"not null" json:"-"`
-	Role     string            `gorm:"default:user" json:"role"`
-
-	FavoritePokemon string     `json:"favorite_pokemon"`
-
-	FavoriteGameID uuid.UUID   `gorm:"type:uuid" json:"favorite_game_id"`
-	FavoriteGame   PokemonGame `gorm:"foreignKey:FavoriteGameID" json:"favorite_game"`
-
-	CreatedAt time.Time        `json:"created_at"`
-	UpdatedAt time.Time        `json:"updated_at"`
-	DeletedAt gorm.DeletedAt   `gorm:"index" json:"deleted_at,omitempty"`
+	ID              uuid.UUID      `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	Username        string         `gorm:"unique;not null" json:"username"`
+	Name            string         `json:"name"`
+	Email           string         `gorm:"unique;not null" json:"email"`
+	Password        string         `gorm:"not null" json:"-"`
+	Roles           []Role         `gorm:"many2many:user_roles;" json:"roles"`
+	FavoritePokemon string         `json:"favorite_pokemon"`
+	FavoriteGameID  uuid.UUID      `gorm:"type:uuid" json:"favorite_game_id"`
+	FavoriteGame    PokemonGame    `gorm:"foreignKey:FavoriteGameID" json:"favorite_game"`
+	CreatedAt       time.Time      `json:"created_at"`
+	UpdatedAt       time.Time      `json:"updated_at"`
+	DeletedAt       gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
 }
 
 type Trainer struct {
@@ -32,4 +28,34 @@ type Trainer struct {
 	Avatar   string
 
 	User     User      `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
+}
+
+
+type Role struct {
+	ID          uuid.UUID    `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	Name        string       `gorm:"unique;not null" json:"name"`
+	Description string       `json:"description"`
+	Permissions []Permission `gorm:"many2many:role_permissions;" json:"permissions"`
+	CreatedAt   time.Time    `json:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+}
+
+type Permission struct {
+	ID          uuid.UUID    `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	Name        string       `gorm:"unique;not null" json:"name"`
+	Description string       `json:"description"`
+	CreatedAt   time.Time    `json:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+}
+
+type RolePermission struct {
+	RoleID       uint `gorm:"primaryKey"`
+	PermissionID uint `gorm:"primaryKey"`
+}
+
+type UserRole struct {
+	UserID uint `gorm:"primaryKey"`
+	RoleID uint `gorm:"primaryKey"`
 }
