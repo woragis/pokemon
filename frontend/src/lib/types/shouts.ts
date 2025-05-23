0b11b709b45e76bb80e855e8c7bbcd912ea62d65
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-23 15:15:01 -0300

    Update shouts type interface

--- frontend/src/lib/types/shouts.ts
+++ frontend/src/lib/types/shouts.ts
@@ -1,7 +1,16 @@
+import type { User } from './user';
+
 export interface Shout {
 	id: string;
+	user_id: string;
+	user: User;
 	content: string;
-	author: string;
 	created_at: string;
 	updated_at: string;
+	reshout_of_id?: string;
+	reshout_of?: Shout;
+	quote_content?: string;
+	likes: any[];
+	comments: any[];
+	is_flagged: boolean;
 }
