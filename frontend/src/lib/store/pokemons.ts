9e24e4c938a3bcd5d2e926df3546c3b6e6e084a7
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-31 00:07:25 -0300

    Separate pokemon colors

--- frontend/src/lib/store/pokemons.ts
+++ frontend/src/lib/store/pokemons.ts
@@ -15,3 +15,23 @@ export const pageNumbers = derived([pagination], ([$pagination]) => {
 
 	return Array.from({ length: end - start + 1 }, (_, i) => start + i);
 });
+export const typeColors: Record<string, string> = {
+	normal: 'bg-gray-300',
+	fire: 'bg-red-500',
+	water: 'bg-blue-500',
+	electric: 'bg-yellow-400',
+	grass: 'bg-green-500',
+	ice: 'bg-blue-200',
+	fighting: 'bg-red-700',
+	poison: 'bg-purple-500',
+	ground: 'bg-yellow-600',
+	flying: 'bg-indigo-300',
+	psychic: 'bg-pink-500',
+	bug: 'bg-green-400',
+	rock: 'bg-yellow-700',
+	ghost: 'bg-purple-700',
+	dragon: 'bg-indigo-600',
+	dark: 'bg-gray-700',
+	steel: 'bg-gray-400',
+	fairy: 'bg-pink-300'
+};
