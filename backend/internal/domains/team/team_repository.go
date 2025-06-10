40f3fe22dc0b751aea23c6394cea48f61fb2d6df
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-10 20:47:43 -0300

    Update team repository to make everything private

--- backend/internal/domains/team/team_repository.go
+++ backend/internal/domains/team/team_repository.go
@@ -9,37 +9,37 @@ import (
  * REPOSITORY INTERFACE *
  ************************/
 
-type TeamRepository interface {
-	Create(team *Team) error
-	GetByID(id uuid.UUID) (*Team, error)
-	ListByUser(userID uuid.UUID, limit int, offset int) ([]Team, error)
-	Update(team *Team) error
-	Delete(id uuid.UUID) error
+type teamRepository interface {
+	create(team *Team) error
+	getByID(id uuid.UUID) (*Team, error)
+	listByUser(userID uuid.UUID, limit int, offset int) ([]Team, error)
+	update(team *Team) error
+	delete(id uuid.UUID) error
 }
 
 /*****************************
  * REPOSITORY IMPLEMENTATION *
  *****************************/
 
-type teamRepository struct {
+type repository struct {
 	db *gorm.DB
 }
 
-func NewTeamRepository(db *gorm.DB) TeamRepository {
-	return &teamRepository{db}
+func NewTeamRepository(db *gorm.DB) teamRepository {
+	return &repository{db}
 }
 
-func (r *teamRepository) Create(team *Team) error {
+func (r *repository) create(team *Team) error {
 	return r.db.Create(team).Error
 }
 
-func (r *teamRepository) GetByID(id uuid.UUID) (*Team, error) {
+func (r *repository) getByID(id uuid.UUID) (*Team, error) {
 	var team Team
 	err := r.db.Preload("Pokemon").First(&team, "id = ?", id).Error
 	return &team, err
 }
 
-func (r *teamRepository) ListByUser(userID uuid.UUID, limit int, offset int) ([]Team, error) {
+func (r *repository) listByUser(userID uuid.UUID, limit int, offset int) ([]Team, error) {
 	var teams []Team
 	err := r.db.
 		Preload("Pokemon").
@@ -50,10 +50,10 @@ func (r *teamRepository) ListByUser(userID uuid.UUID, limit int, offset int) ([]
 	return teams, err
 }
 
-func (r *teamRepository) Update(team *Team) error {
+func (r *repository) update(team *Team) error {
 	return r.db.Save(team).Error
 }
 
-func (r *teamRepository) Delete(id uuid.UUID) error {
+func (r *repository) delete(id uuid.UUID) error {
 	return r.db.Delete(&Team{}, "id = ?", id).Error
 }
