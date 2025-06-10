770f6cfc176b95abb3e56a58a3fe438357bb4317
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-10 16:44:41 -0300

    Create team building repository interface and implementation

---
+++ backend/internal/domains/team/team_repository.go
@@ -0,0 +1,54 @@
+package team
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+/*************
+ * INTERFACE *
+ *************/
+
+type TeamRepository interface {
+	Create(team *Team) error
+	GetByID(id uuid.UUID) (*Team, error)
+	ListByUser(userID uuid.UUID) ([]Team, error)
+	Update(team *Team) error
+	Delete(id uuid.UUID) error
+}
+
+/*****************************
+ * REPOSITORY IMPLEMENTATION *
+ *****************************/
+
+type teamRepository struct {
+	db *gorm.DB
+}
+
+func NewTeamRepository(db *gorm.DB) TeamRepository {
+	return &teamRepository{db}
+}
+
+func (r *teamRepository) Create(team *Team) error {
+	return r.db.Create(team).Error
+}
+
+func (r *teamRepository) GetByID(id uuid.UUID) (*Team, error) {
+	var team Team
+	err := r.db.Preload("Pokemon").First(&team, "id = ?", id).Error
+	return &team, err
+}
+
+func (r *teamRepository) ListByUser(userID uuid.UUID) ([]Team, error) {
+	var teams []Team
+	err := r.db.Preload("Pokemon").Where("user_id = ?", userID).Find(&teams).Error
+	return teams, err
+}
+
+func (r *teamRepository) Update(team *Team) error {
+	return r.db.Save(team).Error
+}
+
+func (r *teamRepository) Delete(id uuid.UUID) error {
+	return r.db.Delete(&Team{}, "id = ?", id).Error
+}
