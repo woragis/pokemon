7a4b02f53f3bcf6eef8064c8c0ad880a8d26a4ad
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-11 16:13:18 -0300

    Create forum entity

--- backend/internal/domains/forum/topic_entity.go
+++ backend/internal/domains/forum/topic_entity.go
@@ -8,11 +8,12 @@ import (
 	"gorm.io/gorm"
 )
 
-type Topic1 struct {
+type Topic struct {
 	ID           uuid.UUID      `json:"id" gorm:"type:uuid;primaryKey;default:generate_uuid_v4()"`
 	UserID       uuid.UUID      `json:"user_id" gorm:"type:uuid;not null;index"`
 	Title        string         `json:"title" gorm:"not null"`
 	Content      string         `json:"content" gorm:"not null"`
+	Pinned       bool           `json:"pinned" gorm:"default:false"`
 
 	ViewCount    int64          `json:"view_count" gorm:"default:0"`
 	CommentCount int64          `json:"comment_count" gorm:"default:0"`
@@ -20,58 +21,21 @@ type Topic1 struct {
 	DislikeCount int64          `json:"dislike_count" gorm:"default:0"`
 
 	User         user.User      `json:"user" gorm:"foreignKey:UserID"`
+
 	CreatedAt    time.Time      `json:"created_at"`
 	UpdatedAt    time.Time      `json:"updated_at"`
     DeletedAt    gorm.DeletedAt `json:"deleted_at"`
 }
 
-type TopicView struct {
-	ID      uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;default:generate_uuid_v4()"`
-	TopicID uuid.UUID `json:"topic_id" gorm:"type:uuid;index;not null"`
-	UserID  uuid.UUID `json:"user_id" gorm:"type:uuid;index"`
-
-	Topic Topic         `json:"topic" gorm:"foreignKey:TopicID"`
-	User  user.User     `json:"user" gorm:"foreignKey:UserID"`
-	CreatedAt time.Time `json:"created_at"`
-}
-
-type TopicLike struct {
-	ID      uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;default:generate_uuid_v4()"`
-	TopicID uuid.UUID `json:"topic_id" gorm:"type:uuid;not null;index:idx_topic_like,unique"`
-	UserID  uuid.UUID `json:"user_id" gorm:"type:uuid;not null;index:idx_topic_like,unique"`
-
-	Like    bool      `json:"like" gorm:"not null"`
-
-	User      user.User `json:"-" gorm:"foreignKey:UserID"`
-	Topic     Topic     `json:"-" gorm:"foreignKey:TopicID"`
-	CreatedAt time.Time `json:"created_at"`
-}
-
-type Comment1 struct {
-	ID           uuid.UUID      `json:"id" gorm:"type:uuid;primaryKey;default:generate_uuid_v4()"`
-	TopicID      uuid.UUID      `json:"topic_id" gorm:"type:uuid;not null;index:idx_topic_like,unique"`
-	UserID       uuid.UUID      `json:"user_id" gorm:"type:uuid;not null;index:idx_topic_like,unique"`
-	ParentID     uuid.UUID      `json:"parent_id,omitempty" gorm:"type:uuid;index"`
-	Content      string         `json:"content" gorm:"type:text;not null"`
-	LikeCount    int64          `json:"like_count" gorm:"default:0"`
-	DislikeCount int64          `json:"dislike_count" gorm:"default:0"`
-	User         user.User      `json:"user" gorm:"foreignKey:UserID"`
-	Topic        Topic          `json:"topic" gorm:"foreignKey:TopicID"`
-	Parent       *Comment1      `json:"parent,omitempty" gorm:"foreignKey:ParentID"`
-	Replies      []Comment1     `json:"replies,omitempty" gorm:"foreignKey:ParentID"`
-	CreatedAt    time.Time      `json:"created_at"`
-	UpdatedAt    time.Time      `json:"updated_at"`
-    DeletedAt    gorm.DeletedAt `json:"deleted_at" gorm:"index"`
-}
-
-type CommentLike struct {
-	ID        uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;default:generate_uuid_v4()"`
-	CommentID uuid.UUID `json:"comment_id" gorm:"type:uuid;not null;index:idx_topic_comment_like,unique"`
-	UserID    uuid.UUID `json:"user_id" gorm:"type:uuid;not null;index:idx_topic_comment_like,unique"`
-
-	Like      bool      `json:"like" gorm:"not null"`
-
-	User      user.User `json:"-" gorm:"foreignKey:UserID"`
-	Comment   Comment1  `json:"-" gorm:"foreignKey:CommentID"`
-	CreatedAt time.Time `json:"created_at"`
+type Response struct {
+	ID           uuid.UUID `json:"id"`
+	Title        string    `json:"title"`
+	Author       string    `json:"author"`
+	AuthorAvatar string    `json:"author_avatar"`
+	Date         string    `json:"date"`
+	Replies      int64     `json:"replies"`
+	Likes        int64     `json:"likes"`
+	Views        int64     `json:"views"`
+	Category     string    `json:"category"`
+	Pinned       bool      `json:"pinned"`
 }
