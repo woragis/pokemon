8ab6e98a7e808ec209758fc23cfd314f3806c0d8
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-11 00:18:09 -0300

    Create cors middleware

---
+++ backend/pkg/utils/cors.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"pokemon/internal/config"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func SetupCors(config *config.Config) fiber.Handler {
+	return cors.New(cors.Config{
+		AllowOrigins:     strings.Join(config.CORSAllowedOrigins, ","),
+		AllowMethods:     strings.Join(config.CORSAllowedMethods, ","),
+		AllowHeaders:     strings.Join(config.CORSAllowedHeaders, ","),
+		AllowCredentials: true,
+		MaxAge:           3600,
+	})
+}
