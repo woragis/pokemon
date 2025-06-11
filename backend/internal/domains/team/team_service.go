01b34770402f5f925b50e8242db20a6700b4b9ec
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-10 21:03:43 -0300

    Make new service be private

--- backend/internal/domains/team/team_service.go
+++ backend/internal/domains/team/team_service.go
@@ -39,7 +39,7 @@ type service struct {
 	redis *redis.Client
 }
 
-func NewTeamService(repo teamRepository, redis *redis.Client) teamService {
+func newTeamService(repo teamRepository, redis *redis.Client) teamService {
 	return &service{repo: repo, redis: redis}
 }
 
