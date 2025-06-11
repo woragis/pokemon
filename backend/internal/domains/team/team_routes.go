8e13b9240183c9662b998911f641d7c80cb6faa8
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-11 00:09:15 -0300

    Implement interaction routes to team

--- backend/internal/domains/team/team_routes.go
+++ backend/internal/domains/team/team_routes.go
@@ -6,14 +6,38 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func (handler *handler) RegisterRoutes(app fiber.Router) {
+func (h *handler) RegisterRoutes(app fiber.Router) {
 	teamGroup := app.Group("/teams")
 
-	teamGroup.Get("/:id", handler.getTeam)
-	teamGroup.Get("/user/:user_id", handler.listTeams)
+	// Public routes
+	teamGroup.Get("/:id", h.getTeam)
+	teamGroup.Get("/user/:user_id", h.listTeams)
+	teamGroup.Get("/:id/comments", h.getTeamComments)
+	teamGroup.Get("/:id/comments/count", h.getTeamCommentCount)
+	teamGroup.Get("/:id/views/count", h.getTeamViewCount)
+	teamGroup.Get("/:id/likes/count", h.getTeamLikeCount)
 
+	// Auth required
 	teamGroup.Use(middleware.AuthRequired())
-	teamGroup.Post("/", handler.createTeam)
-	teamGroup.Put("/:id", handler.updateTeam)
-	teamGroup.Delete("/:id", handler.deleteTeam)
+
+	// Team CRUD
+	teamGroup.Post("/", h.createTeam)
+	teamGroup.Put("/:id", h.updateTeam)
+	teamGroup.Delete("/:id", h.deleteTeam)
+
+	// Interaction routes
+	teamGroup.Post("/:id/comments", h.commentTeam)
+	teamGroup.Put("/comments/:comment_id", h.updateComment)
+	teamGroup.Delete("/comments/:comment_id", h.deleteComment)
+
+	teamGroup.Post("/:id/like", h.likeTeam)
+	teamGroup.Delete("/:id/like", h.unlikeTeam)
+	teamGroup.Get("/:id/likes", h.isTeamLikedByUser)
+
+	teamGroup.Post("/:id/save", h.saveTeam)
+	teamGroup.Delete("/:id/save", h.unsaveTeam)
+	teamGroup.Get("/saved", h.getSavedTeams)
+	teamGroup.Get("/:id/saved", h.isTeamSavedByUser)
+
+	teamGroup.Post("/:id/view", h.viewTeam)
 }
