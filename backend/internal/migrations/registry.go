dcb42e5ae0bc72ea097c5b93936acfe13b4fc510
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-13 13:04:32 -0300

    Add migrations to registry

--- backend/internal/migrations/registry.go
+++ backend/internal/migrations/registry.go
@@ -1,11 +1,17 @@
 package migrations
 
 import (
+	favoritepokemon "pokemon/internal/domains/favorite-pokemon"
+	"pokemon/internal/domains/forum"
+	"pokemon/internal/domains/team"
 	"pokemon/internal/domains/user"
 )
 
 func GetAllMigrators() []Migrator {
 	return []Migrator{
 		user.UserMigrator{},
+		forum.ForumMigrator{},
+		team.TeamMigrator{},
+		favoritepokemon.FavoritePokemonMigrator{},
 	}
 }
