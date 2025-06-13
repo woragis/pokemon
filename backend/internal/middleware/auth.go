f04f80dfe6e663bf055eeee86cc9e44f6fda5b94
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-12 22:59:04 -0300

    Add auth optional middleware

--- backend/internal/middleware/auth.go
+++ backend/internal/middleware/auth.go
@@ -26,3 +26,23 @@ func AuthRequired() fiber.Handler {
         return c.Next()
     }
 }
+
+func AuthOptional() fiber.Handler {
+    return func(c *fiber.Ctx) error {
+        authHeader := c.Get("Authorization")
+        if authHeader == "" {
+            return c.Next()
+        }
+        
+        tokenString := strings.Replace(authHeader, "Bearer ", "", 1)
+        claims, err := utils.ValidateJWT(tokenString)
+        if err != nil {
+            return c.Next()
+        }
+        
+        c.Locals("userID", claims.UserID)
+        c.Locals("email", claims.Email)
+        
+        return c.Next()
+    }
+}
