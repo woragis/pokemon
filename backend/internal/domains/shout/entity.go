3b27f99aedcd956e7df43982f351caccfc8e396f
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-15 23:18:08 -0300

    Create shout entities and validations

--- backend/internal/domains/shout/entity.go
+++ backend/internal/domains/shout/entity.go
@@ -1,7 +1,9 @@
 package shout
 
 import (
+	"errors"
 	"pokemon/internal/domains/user"
+	"pokemon/pkg/validation"
 	"time"
 
 	"github.com/google/uuid"
@@ -60,4 +62,91 @@ type ShoutCommentLike struct {
 	UserID    uuid.UUID    `gorm:"type:uuid;not null;index;uniqueIndex:idx_user_comment_like" json:"user_id"`
 	CommentID uuid.UUID    `gorm:"type:uuid;not null;index;uniqueIndex:idx_user_comment_like" json:"comment_id"`
 	CreatedAt time.Time    `json:"created_at"`
+}
+
+type ShoutSave struct {
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	UserID    uuid.UUID `gorm:"type:uuid;not null;index;uniqueIndex:idx_user_shout_save" json:"user_id"`
+	ShoutID   uuid.UUID `gorm:"type:uuid;not null;index;uniqueIndex:idx_user_shout_save" json:"shout_id"`
+	CreatedAt time.Time `json:"created_at"`
+
+	User  user.User `gorm:"foreignKey:UserID" json:"user"`
+	Shout Shout     `gorm:"foreignKey:ShoutID" json:"shout"`
+}
+
+type ShoutView struct {
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	UserID    uuid.UUID `gorm:"type:uuid" json:"user_id,omitempty"`
+	ShoutID   uuid.UUID `gorm:"type:uuid;not null;index" json:"shout_id"`
+	CreatedAt time.Time `json:"created_at"`
+
+	User  user.User `gorm:"foreignKey:UserID" json:"user"`
+	Shout Shout     `gorm:"foreignKey:ShoutID" json:"shout"`
+}
+
+func (s *Shout) Validate() error {
+	if err := validation.UUIDRequired(s.UserID, "user_id"); err != nil {
+		return err
+	}
+	if err := validation.StringRequired(s.Content, "content"); err != nil {
+		return err
+	}
+	if len(s.Content) > 280 {
+		return errors.New("content must be 280 characters or less")
+	}
+	if s.QuoteContent != nil && len(*s.QuoteContent) > 280 {
+		return errors.New("quote_content must be 280 characters or less")
+	}
+	return nil
+}
+
+func (c *ShoutComment) Validate() error {
+	if err := validation.UUIDRequired(c.UserID, "user_id"); err != nil {
+		return err
+	}
+	if err := validation.UUIDRequired(c.ShoutID, "shout_id"); err != nil {
+		return err
+	}
+	if err := validation.StringRequired(c.Content, "content"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (l *ShoutLike) Validate() error {
+	if err := validation.UUIDRequired(l.UserID, "user_id"); err != nil {
+		return err
+	}
+	if err := validation.UUIDRequired(l.ShoutID, "shout_id"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (l *ShoutCommentLike) Validate() error {
+	if err := validation.UUIDRequired(l.UserID, "user_id"); err != nil {
+		return err
+	}
+	if err := validation.UUIDRequired(l.CommentID, "comment_id"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *ShoutSave) Validate() error {
+	if err := validation.UUIDRequired(s.UserID, "user_id"); err != nil {
+		return err
+	}
+	if err := validation.UUIDRequired(s.ShoutID, "shout_id"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (v *ShoutView) Validate() error {
+	if err := validation.UUIDRequired(v.ShoutID, "shout_id"); err != nil {
+		return err
+	}
+	// UserID is optional
+	return nil
 }
