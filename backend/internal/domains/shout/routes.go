afc1d8384658c3315532868e908d544e70981cd1
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-15 23:31:52 -0300

    Create shout interactions routes

--- backend/internal/domains/shout/routes.go
+++ backend/internal/domains/shout/routes.go
@@ -8,14 +8,35 @@ import (
 
 func (h *handler) RegisterRoutes(app fiber.Router) {
 	shouts := app.Group("/shouts")
+
+	// Public routes
 	shouts.Get("/", h.listShouts)
 	shouts.Get("/:id", h.getShout)
 	shouts.Get("/user/:userID", h.listShoutsByUser)
 	shouts.Get("/parent/:reshoutID", h.listShoutsByParent)
 
+	// Interaction routes (public)
+	shouts.Get("/:shout_id/interactions", h.getInteractions)
+	shouts.Post("/:shout_id/views", middleware.AuthOptional(), h.createView)
+
+	// Authenticated routes
 	shouts.Use(middleware.AuthRequired())
+
 	shouts.Post("/", h.createShout)
 	shouts.Post("/:id/reshout", h.retweetShout)
 	shouts.Put("/:id", h.updateShout)
 	shouts.Delete("/:id", h.deleteShout)
+
+	// Likes
+	shouts.Post("/:shout_id/likes", h.createLike)
+	shouts.Delete("/:shout_id/likes", h.deleteLike)
+
+	// Comments
+	shouts.Post("/comments", h.createComment)
+	shouts.Put("/comments", h.updateComment)
+	shouts.Delete("/comments/:comment_id", h.deleteComment)
+
+	// Saves
+	shouts.Post("/:shout_id/saves", h.createSave)
+	shouts.Delete("/:shout_id/saves", h.deleteSave)
 }
