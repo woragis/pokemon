93d09595970d63191ba28e495fe9a7e4373a71e7
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-19 22:45:33 -0300

    Update news routes

--- backend/internal/domains/news/routes.go
+++ backend/internal/domains/news/routes.go
@@ -1,6 +1,10 @@
 package news
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"pokemon/internal/middleware"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func (h *handler) RegisterRoutes(app fiber.Router) {
 	news := app.Group("/news")
@@ -9,9 +13,11 @@ func (h *handler) RegisterRoutes(app fiber.Router) {
 	news.Get("/:id", h.get)
 	news.Get("/user/:user_id", h.listByUser)
 
+	// news.Post("/:id/view", middleware.AuthOptional(), h.createView)
+
+	news.Use(middleware.AuthRequired())
+
 	news.Post("/", h.create)
 	news.Put("/:id", h.update)
 	news.Delete("/:id", h.delete)
-
-	// news.Post("/:id/view", h.createView)
 }
