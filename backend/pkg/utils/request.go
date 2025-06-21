01c3af3523e367801269ba6a0a1905affd227106
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-20 23:20:30 -0300

    Create parse uuid utils

--- backend/pkg/utils/request.go
+++ backend/pkg/utils/request.go
@@ -1,6 +1,9 @@
 package utils
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
 
 func ParsePagination(c *fiber.Ctx) (limit, offset int) {
 	limit = 20
@@ -13,4 +16,13 @@ func ParsePagination(c *fiber.Ctx) (limit, offset int) {
 		offset = o
 	}
 	return
+}
+
+// ParseUUID parses a string into a UUID. If invalid, returns uuid.Nil (000...000).
+func ParseUUID(id string) uuid.UUID {
+	parsed, err := uuid.Parse(id)
+	if err != nil {
+		return uuid.Nil
+	}
+	return parsed
 }
